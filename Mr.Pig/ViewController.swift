599621f27a065e8080eac35eac05396f50aac09d
Larry Petroski <larry.petroski@yahoo.com>
2018-07-10 18:19:31 -0500

    Part 1

--- Mr.Pig/ViewController.swift
+++ Mr.Pig/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import SceneKit
+import SpriteKit
 
 class ViewController: UIViewController {
+    
+    let game = GameHelper.sharedInstance
+    
+    var scnView:SCNView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupScenes()
+        setupNodes()
+        setupActions()
+        setupTraffic()
+        setupGestures()
+        setupSounds()
+        
+        game.state = .tapToPlay
+    }
+    
+    func setupScenes() {
+        scnView = SCNView(frame: self.view.frame)
+        self.view.addSubview(scnView)
+    }
+    
+    func setupNodes() {
+    }
+    
+    func setupActions() {
+    }
+    
+    func setupTraffic() {
+    }
+    
+    func setupGestures() {
+    }
+    
+    func setupSounds() {
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override var prefersStatusBarHidden: Bool { return true }
+    override var shouldAutorotate: Bool { return false }
 }
-
